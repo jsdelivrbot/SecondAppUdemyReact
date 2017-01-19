96e32815718c0ee7f1ec8a12bb200f2956e7db33
Vanessa Pacheco <vpacheco@isf.com.co>
2017-01-19 14:09:50 -0500

    Second App

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,8 @@
+export default function(){
+    return [
+        {title: 'Javascript', pages: 455},
+        {title: 'React', pages: 150},
+        {title: 'Python', pages: 784},
+        {title: 'Ruby', pages: 100}
+    ]
+}
